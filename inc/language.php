b113a90aff1ffa3ffae4e24971d0321dd97cbf99
Alexander <oleksandr.kulachinsky@lengin.com>
2024-11-29 19:18:28 +0100

    update checked translated strings

--- inc/language.php
+++ inc/language.php
@@ -3,14 +3,14 @@ $domain     = 'fungi';
 $action_tag = 'wpml_register_single_string';
 
 // buttons
-do_action( $action_tag, $domain, 'Buttons - Subscribe now', 'Subscribe now' );
+// do_action( $action_tag, $domain, 'Buttons - Subscribe now', 'Subscribe now' );
 do_action( $action_tag, $domain, 'buttons - Load more', 'Load more' );
 do_action( $action_tag, $domain, 'buttons - Learn more', 'Learn more' );
 do_action( $action_tag, $domain, 'buttons - Read more', 'Read more' );
-do_action( $action_tag, $domain, 'buttons - Read completely', 'Read completely' );
+// do_action( $action_tag, $domain, 'buttons - Read completely', 'Read completely' );
 do_action( $action_tag, $domain, 'buttons - Send', 'Send' );
 do_action( $action_tag, $domain, 'buttons - In store', 'In store' );
-do_action( $action_tag, $domain, 'buttons - Buy in 1 click', 'Buy in 1 click' );
+// do_action( $action_tag, $domain, 'buttons - Buy in 1 click', 'Buy in 1 click' );
 do_action( $action_tag, $domain, 'buttons - Add to cart', 'Add to cart' );
 do_action( $action_tag, $domain, 'buttons - to main page', 'To main page' );
 
@@ -21,13 +21,11 @@ do_action( $action_tag, $domain, 'Footer - Contact Us', 'Contact Us' );
 do_action( $action_tag, $domain, 'Footer - Learn more', 'Learn more' );
 do_action( $action_tag, $domain, 'Footer - Join Our Newsletter', 'Join Our Newsletter' );
 do_action( $action_tag, $domain, 'Footer - noties under subscribe', 'You may withdraw your consent at any time!' );
-do_action( $action_tag, $domain, 'Footer - marketplace', 'Connect to the affiliate program "Marketplace"' );
 
 
 
 // header
 do_action( $action_tag, $domain, 'Header - free shipping', 'FREE SHIPPING ON ORDERS OVER $125' );
-do_action( $action_tag, $domain, 'Header - catalog', 'Catalog' );
 
 
 
@@ -45,14 +43,16 @@ do_action( $action_tag, $domain, 'Sidebar - posts title', 'Popular Post' );
 do_action( $action_tag, $domain, 'Sidebar - pages title', 'Related pages' );
 
 
+// Mobile menu
+do_action( $action_tag, $domain, 'Mobile menu - Main page', 'Main page' );
+do_action( $action_tag, $domain, 'Mobile menu - Delivery and payment', 'Delivery and payment' );
+do_action( $action_tag, $domain, 'Mobile menu - Returns and exchanges', 'Returns and exchanges' );
+do_action( $action_tag, $domain, 'Mobile menu - Call us', 'Call us' );
+do_action( $action_tag, $domain, 'Mobile menu - Reviews', 'Reviews' );
+do_action( $action_tag, $domain, 'Mobile menu - catalog', 'Catalog' );
+
+
 // Modals
-// do_action( $action_tag, $domain, 'Modals - Menu', 'Menu' );
-do_action( $action_tag, $domain, 'Modals - Main page', 'Main page' );
-do_action( $action_tag, $domain, 'Modals - About us', 'About us' );
-do_action( $action_tag, $domain, 'Modals - Delivery and payment', 'Delivery and payment' );
-do_action( $action_tag, $domain, 'Modals - Returns and exchanges', 'Returns and exchanges' );
-do_action( $action_tag, $domain, 'Modals - Call us', 'Call us' );
-do_action( $action_tag, $domain, 'Modals - Reviews', 'Reviews' );
 do_action( $action_tag, $domain, 'Modals - Catalog', 'Catalog' );
 do_action( $action_tag, $domain, 'Modals - Scale a photo', 'Scale a photo with a pinch and spread gesture' );
 
@@ -63,16 +63,12 @@ do_action( $action_tag, $domain, 'input - Name', 'Name' );
 do_action( $action_tag, $domain, 'input - Surname', 'Surname' );
 do_action( $action_tag, $domain, 'input - Phone', 'Phone' );
 do_action( $action_tag, $domain, 'input - City', 'City' );
-do_action( $action_tag, $domain, 'input - Post Office', 'Post Office' );
 do_action( $action_tag, $domain, 'input - Promo code', 'Promo code' );
 do_action( $action_tag, $domain, 'input - Your comment', 'Your comment' );
 
 
 // Cart
 do_action( $action_tag, $domain, 'Cart - Cart', 'Cart' );
-do_action( $action_tag, $domain, 'Cart - Brand', 'Brand' );
-do_action( $action_tag, $domain, 'Cart - Size', 'Size' );
-do_action( $action_tag, $domain, 'Cart - Color', 'Color' );
 do_action( $action_tag, $domain, 'Cart - Quantity', 'Quantity' );
 do_action( $action_tag, $domain, 'Cart - Remove', 'Remove' );
 do_action( $action_tag, $domain, 'Cart - Cart is empty', 'Cart is empty' );
@@ -87,38 +83,38 @@ do_action( $action_tag, $domain, 'Cart - Go to cart', 'Go to cart' );
 
 
 // Checkout
-do_action( $action_tag, $domain, 'Checkout - Thank you', 'Thank you. Your order has been accepted.' );
-do_action( $action_tag, $domain, 'Checkout - Order number', 'Order number' );
-do_action( $action_tag, $domain, 'Checkout - Date', 'Date' );
-do_action( $action_tag, $domain, 'Checkout - City', 'City' );
-do_action( $action_tag, $domain, 'Checkout - Post Office', 'Post Office' );
-do_action( $action_tag, $domain, 'Checkout - Payment method', 'Payment method' );
-do_action( $action_tag, $domain, 'Checkout - Full name', 'Full name' );
-do_action( $action_tag, $domain, 'Checkout - Phone', 'Phone' );
-do_action( $action_tag, $domain, 'Checkout - Ordering information', 'Ordering information' );
-do_action( $action_tag, $domain, 'Checkout - Price', 'Price' );
-do_action( $action_tag, $domain, 'Checkout - Ordering', 'Ordering' );
+// do_action( $action_tag, $domain, 'Checkout - Thank you', 'Thank you. Your order has been accepted.' );
+// do_action( $action_tag, $domain, 'Checkout - Order number', 'Order number' );
+// do_action( $action_tag, $domain, 'Checkout - Date', 'Date' );
+// do_action( $action_tag, $domain, 'Checkout - City', 'City' );
+// do_action( $action_tag, $domain, 'Checkout - Post Office', 'Post Office' );
+// do_action( $action_tag, $domain, 'Checkout - Payment method', 'Payment method' );
+// do_action( $action_tag, $domain, 'Checkout - Full name', 'Full name' );
+// do_action( $action_tag, $domain, 'Checkout - Phone', 'Phone' );
+// do_action( $action_tag, $domain, 'Checkout - Ordering information', 'Ordering information' );
+// do_action( $action_tag, $domain, 'Checkout - Price', 'Price' );
+// do_action( $action_tag, $domain, 'Checkout - Ordering', 'Ordering' );
 
 
 // Product
 // do_action( $action_tag, $domain, 'Product - Other colors', 'Other colors' );
 do_action( $action_tag, $domain, 'Product - Out of stock', 'Out of stock' );
 do_action( $action_tag, $domain, 'Product - In stock', 'In stock' );
-do_action( $action_tag, $domain, 'Product - Description', 'Description' );
-do_action( $action_tag, $domain, 'Product - SKU', 'SKU' );
+// do_action( $action_tag, $domain, 'Product - Description', 'Description' );
+// do_action( $action_tag, $domain, 'Product - SKU', 'SKU' );
 do_action( $action_tag, $domain, 'Product - Related products', 'Related products' );
 do_action( $action_tag, $domain, 'Product - Reviews', 'Reviews' );
 // do_action( $action_tag, $domain, 'Product - Size chart', 'Size chart' );
-do_action( $action_tag, $domain, 'Product - Brand', 'Brand' );
-do_action( $action_tag, $domain, 'Product - Category', 'Category' );
+// do_action( $action_tag, $domain, 'Product - Brand', 'Brand' );
+// do_action( $action_tag, $domain, 'Product - Category', 'Category' );
 do_action( $action_tag, $domain, 'Product - What’s Inside', 'What’s Inside' );
 do_action( $action_tag, $domain, 'Product - How to use', 'How to use?' );
-do_action( $action_tag, $domain, 'Product - Availability', 'Availability' );
-do_action( $action_tag, $domain, 'Product - Delivery', 'Delivery' );
+// do_action( $action_tag, $domain, 'Product - Availability', 'Availability' );
+// do_action( $action_tag, $domain, 'Product - Delivery', 'Delivery' );
 do_action( $action_tag, $domain, 'Product - Sertificates', 'Sertificates' );
-do_action( $action_tag, $domain, 'Product - Sending on tomorrow', 'Sending on tomorrow' );
-do_action( $action_tag, $domain, 'Product - Delivery and returns', 'Delivery and returns' );
-do_action( $action_tag, $domain, 'Product - Payment method', 'Payment method' );
+// do_action( $action_tag, $domain, 'Product - Sending on tomorrow', 'Sending on tomorrow' );
+// do_action( $action_tag, $domain, 'Product - Delivery and returns', 'Delivery and returns' );
+// do_action( $action_tag, $domain, 'Product - Payment method', 'Payment method' );
 
 
 
